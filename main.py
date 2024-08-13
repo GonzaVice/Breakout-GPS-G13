309aa368abbdc23b778b4b7a401251202fb141a1
GonzaVice <givicente@miuandes.cl>
2024-08-13 13:24:33 -0400

    Create gameloop

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Breakout")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((320,240))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill("purple")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+pygame.quit()
