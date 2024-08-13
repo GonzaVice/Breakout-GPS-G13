fb2c56a8824b94fe05e6517893a6631313560f91
GonzaVice <givicente@miuandes.cl>
2024-08-13 14:53:07 -0400

    Expanding window resolution while retaining internal resolution

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from settings import WIDTH, HEIGHT, TITLE, FPS
 from paddle import Paddle
 from ball import Ball
 
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 720
+
 def main():
 
     # Setting inicial
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption(TITLE)
     clock = pygame.time.Clock()
     running = True
+
+    # Resolución interna
+    render_surface = pygame.Surface((WIDTH, HEIGHT))
+    scale_x = WINDOW_WIDTH / WIDTH
+    scale_y = WINDOW_HEIGHT / HEIGHT
 
     # Jugador
     paddle = Paddle(x=WIDTH//2 - 16, y=HEIGHT - 20)
@@ -29,9 +37,13 @@ def main():
         ball.check_collision(paddle)
 
         # Dibujo por frame
-        screen.fill("black")
-        paddle.draw(screen)
-        ball.draw(screen)
+        render_surface.fill("black")
+        paddle.draw(render_surface)
+        ball.draw(render_surface)
+
+        # Escalar al tamaño de la ventana
+        scaled_surface = pygame.transform.scale(render_surface, (int(WIDTH * scale_x), int(HEIGHT * scale_y)))
+        screen.blit(scaled_surface, (0, 0))
 
         pygame.display.flip()
         clock.tick(FPS)
